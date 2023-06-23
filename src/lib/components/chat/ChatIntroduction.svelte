c11433d46f8a168ce5a0f6e2ba4fcba764375d47
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-06-23 12:34:12 +0200

    adapter & netlify toml

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -31,10 +31,10 @@
 	<div class="lg:col-span-1">
 		<div>
 			<div class="mb-3 flex items-center text-2xl font-semibold">
-				<Logo classNames="mr-1 flex-none" />
+				<Logo classNames="mr-4" />
 				{PUBLIC_APP_NAME}
 				<div
-					class="ml-3 flex h-6 items-center rounded-lg border border-gray-100 bg-gray-50 px-2 text-base text-gray-400 dark:border-gray-700/60 dark:bg-gray-800"
+					class="ml-6 flex h-6 items-center rounded-lg border border-gray-100 bg-gray-50 px-2 text-base text-gray-400 dark:border-gray-700/60 dark:bg-gray-800"
 				>
 					v{PUBLIC_VERSION}
 				</div>
