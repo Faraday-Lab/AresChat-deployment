818d9d47c2a97586025c5bedd4efd93ea7ef8ad3
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-07-05 11:00:15 +0200

    added dismiss and deletion handling

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -49,8 +49,6 @@
         message = "";
     };
 
-    export let isLoading = false;
-
     function chatWithPDF() {
         clickFileUpload();
         addChangeEventListener();
@@ -74,23 +72,20 @@
                     "x-api-key": API_KEY,
                 },
             };
-            isLoading = true;
-            alertBoxProcessing();
 
-            axiosPostRequest(formData, options);
+            alertBoxProcessing("Uploading");
 
-        } else {
-            isLoading = false;
+            uploadFile(formData, options);
         }
     }
 
-    function axiosPostRequest(formData, options) {
+    function uploadFile(formData, options) {
         axios
             .post("https://api.chatpdf.com/v1/sources/add-file", formData, options)
             .then((response) => {
                 console.log("Source ID:", response.data.sourceId);
-                isLoading = false;
-                alertSuccess();
+                const sourceID = response.data.sourceId;
+                alertSuccess(sourceID);
             })
             .catch((error) => {
                 console.log("Error:", error.message);
@@ -98,21 +93,27 @@
             })
     }
 
-    function alertSuccess() {
+    function alertSuccess(sourceID) {
         Swal.fire({
             icon: 'success',
-            text: 'File uploaded successfully!',
+            html: 'File uploaded successfully!<br>You can now chat with your file!',
             showConfirmButton: true,
-            footer: 'You can now chat with your file!',
             background: '#1f2937',
             color: 'white',
-            confirmButtonColor: '#059669'
+            confirmButtonColor: '#059669',
+            confirmButtonText: "Great!",
+            showDenyButton: true,
+            denyButtonText: 'Dismiss my file'
+        }).then((result) => {
+            if (result.isDenied) {
+                isDenied(sourceID);
+            }
         });
     }
 
-    function alertBoxProcessing() {
+    function alertBoxProcessing(value: string) {
         Swal.fire({
-            title: 'Uploading your file...',
+            title: `${value} your file...`,
             background: '#1f2937',
             allowOutsideClick: false,
             customClass: {
@@ -124,8 +125,48 @@
                 Swal.showLoading(Swal.getConfirmButton());
             }
         });
-
     }
+
+    function isDenied(sourceID) {
+        const options = {
+            headers: {
+                "x-api-key": API_KEY,
+                "Content-Type": "application/json"
+            }
+        };
+
+        const data = {
+            sources: [sourceID.toString()]
+        };
+
+        console.log(data);
+
+        alertBoxProcessing("Deleting");
+
+        axios
+            .post("https://api.chatpdf.com/v1/sources/delete", data, options)
+            .then(() => {
+                confirmDeletion();
+            })
+            .catch((error) => {
+                console.log("Error:", error.message);
+                console.log("Response:", error.response?.data);
+            });
+    }
+
+    function confirmDeletion() {
+        Swal.fire({
+            icon: 'success',
+            text: 'File deleted successfully!',
+            showConfirmButton: true,
+            background: '#1f2937',
+            color: 'white',
+            confirmButtonColor: '#059669',
+            confirmButtonText: "Thank you!",
+            allowOutsideClick: true
+        });
+    }
+
 </script>
 
 <div class="relative min-h-0 min-w-0">
@@ -210,26 +251,13 @@
             </div>
             <div class="flex flex-row items-center justify-center space-x-3 mr-9 mt-1">
                 <div>
-                    {#if isLoading}
-                        <div class="status">
-                            <svg aria-hidden="true"
-                                 class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-emerald-700 font-semibold"
-                                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                      fill="currentColor"/>
-                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                      fill="currentFill"/>
-                            </svg>
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    {:else}
-                        <form enctype="multipart/form-data" id="fileUploadForm">
-                            <input type="file" name="fileUpload" id="fileUpload" style="display: none;">
-                            <button type="button" on:click={chatWithPDF}>
-                                <img class="h-4 w-4" src="../chatui/file.png" alt="upload a file">
-                            </button>
-                        </form>
-                    {/if}
+
+                    <form enctype="multipart/form-data" id="fileUploadForm">
+                        <input type="file" name="fileUpload" id="fileUpload" style="display: none;">
+                        <button type="button" on:click={chatWithPDF}>
+                            <img class="h-4 w-4" src="../chatui/file.png" alt="upload a file">
+                        </button>
+                    </form>
                 </div>
 
                 <div>
