ec6baa30409089e6b6a08ae95987eae34d69bade
Ngniguepa Therence <111742624+Ngniguepatherence@users.noreply.github.com>
2023-07-08 23:09:34 +0100

    Update +page.svelte

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -28,7 +28,6 @@
 <div class="min-h-screen p-6 bg-dark-100 flex items-center justify-center">
     <div class="container max-w-screen-lg mx-auto">
       <div>
-        <img src="../chatui/vert_bleu.svg" class="h-32" alt="logo"/>
         <h1 class="text-5xl font-bold  mt-1">Best AI of the world</h1>
         <div class="bg-white rounded border rounded shadow-lg p-4 px-4 md:p-8 mb-6">
           <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
