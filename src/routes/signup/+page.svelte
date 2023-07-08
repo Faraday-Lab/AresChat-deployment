0213ef9b0e945b5457cc6cbaf22b0c92524428b5
Ngniguepa Therence <111742624+Ngniguepatherence@users.noreply.github.com>
2023-07-08 23:16:35 +0100

    Update +page.svelte

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -28,12 +28,11 @@
 <div class="min-h-screen p-6 bg-dark-100 flex items-center justify-center">
     <div class="container max-w-screen-lg mx-auto">
       <div>
-        <h1 class="text-5xl font-bold  mt-1">AresChat</h1>
         <div class="bg-white rounded border rounded shadow-lg p-4 px-4 md:p-8 mb-6">
           <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
             <div class="text-gray-600">
-              <p class="font-medium text-lg">Create A New Account</p>
-              <p>Please complete all informations to signUp</p>
+              <p class="font-medium text-lg">AresChat | Create A New Account</p>
+              <p>Welcome,please complete all informations to signup</p>
               <p class="flex items-center justify-center mt-4 mb-4">OR</p>
               <a href={`${base}/`} class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                 <div class="px-4 py-3">
