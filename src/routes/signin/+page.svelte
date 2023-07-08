e2863990909993900ee9db0eaaa0ba60f373ff53
Ngniguepa Therence <111742624+Ngniguepatherence@users.noreply.github.com>
2023-07-08 21:32:16 +0100

    Update +page.svelte

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { onMount } from "svelte";
-
+    import {base} from "$app/paths";
+    import {createEventDispatcher} from "svelte";
+    
     let username = "";
     let password = "";
     let isRegisterPage = false;
@@ -12,6 +14,9 @@
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
             },
             body: JSON.stringify({username,password }),
         });
@@ -39,74 +44,69 @@
     }
   </script>
 <style>
-    main {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: transparent;
-  font-family: Arial, sans-serif;
-}
-
-.login-container {
-  background-color: #ffffff;
-  border-radius: 4px;
-  padding: 40px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-}
-
-h1 {
-  margin-bottom: 30px;
-  color: #333333;
-}
-
-form input {
-  width: 100%;
-  padding: 12px;
-  margin-bottom: 20px;
-  border: 1px solid #cccccc;
-  border-radius: 4px;
-}
-
-form button {
-  width: 100%;
-  padding: 12px;
-  background-color: #4CAF50;
-  color: #ffffff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-form button:hover {
-  background-color: #45a049;
-}
-
-p a {
-  color: #333333;
-  text-decoration: none;
-}
-
-p a:hover {
-  text-decoration: underline;
-}
+    /* * {
+        box-sizing: border-box;
+        border: 0 solid #e5e7eb;
+    }
+    div {
+        display: block;
+    } */
+    
+    
 
 </style>
-
-<main class="w-4/5 flex justify-center mx-auto">
-    <div class="flex flex-col items-center">
-        <img src="../chatui/vert_bleu.svg" class="h-32" alt="logo"/>
-       <h1 class="text-5xl font-semibold text-emerald-600 mt-4">Best AI of the world</h1>
+<!-- https://play.tailwindcss.com/MIwj5Sp9pw -->
+<div class="py-16">
+    <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+        <div class="hidden lg:block lg:w-1/2 bg-cover"
+            style="background-image:url('https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80')">
+        </div>
+        <div class="w-full p-8 lg:w-1/2">
+            <h2 class="text-2xl font-semibold text-gray-700 text-center">AresChat</h2>
+            <p class="text-xl text-gray-600 text-center">Welcome back!</p>
+            <a href="#" class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
+                <div class="px-4 py-3">
+                    <svg class="h-6 w-6" viewBox="0 0 40 40">
+                        <path
+                            d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                            fill="#FFC107" />
+                        <path
+                            d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                            fill="#FF3D00" />
+                        <path
+                            d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                            fill="#4CAF50" />
+                        <path
+                            d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                            fill="#1976D2" />
+                    </svg>
+                </div>
+                <h1 class="px-4 py-3 w-5/6 text-center text-gray-600 font-bold">Sign in with Google</h1>
+            </a>
+            <div class="mt-4 flex items-center justify-between">
+                <span class="border-b w-1/5 lg:w-1/4"></span>
+                <a href="#" class="text-xs text-center text-gray-500 uppercase">or login with email</a>
+                <span class="border-b w-1/5 lg:w-1/4"></span>
+            </div>
+            <div class="mt-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" />
+            </div>
+            <div class="mt-4">
+                <div class="flex justify-between">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                    <a href="#" class="text-xs text-gray-500">Forget Password?</a>
+                </div>
+                <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" />
+            </div>
+            <div class="mt-8">
+                <button class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
+            </div>
+            <div class="mt-4 flex items-center justify-between">
+                <span class="border-b w-1/5 md:w-1/4"></span>
+                <a href={`${base}/signup`} class="text-xs text-gray-500 uppercase">or sign up</a>
+                <span class="border-b w-1/5 md:w-1/4"></span>
+            </div>
+        </div>
     </div>
-    <div class="login-container">
-        <h1 class="text-5xl font-semibold text-emerald-600 mt-4">Login Page</h1>
-        <form on:submit|preventDefault={handleSubmit}>
-          <input type="text" bind:value={username} placeholder="Username" required>
-          <input type="password" bind:value={password} placeholder="Password" required>
-          <button type="submit">Login</button>
-        </form>
-        <p><a href="#">Mot de passe oublié ?</a></p>
-        <p class="m-4">Don't have a account?  <a href="#">Register</a></p>
-      </div>
-</main>
+</div>
