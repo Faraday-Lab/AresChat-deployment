a7e0e20ed95aaa5826eba8049a3d275930789937
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-07-12 13:37:44 +0200

    text to image

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -346,47 +346,47 @@
                 .catch((error) => {
                 console.error(error);
                 });
-			}
-            preConfirm: (prompt) => {
-                return fetch(
-                    `https://api.stability.ai/v1/generation/stable-diffusion-v1-5/text-to-image`,
-                    {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            Accept: 'application/json',
-                            Authorization: `sk-bMhuMy55knbw133npsyeXP78HOe2TKMArEGlbueyxiAxaWED`,
-                        },
-                        body: JSON.stringify({
-                            text_prompts: [
-                                {
-                                    text: prompt
-                                },
-                            ],
-                            cfg_scale: 7,
-                            clip_guidance_preset: 'FAST_BLUE',
-                            height: 512,
-                            width: 512,
-                            samples: 3,
-                            steps: 30,
-                        }),
-                    }
-                )
-                    .then((response) => {
-                        if (!response.ok) {
-                            throw new Error(`Non-200 response: ${response.text()}`);
-                        }
-                        return response.json();
-                    })
-                    .then((responseJSON) => {
-                        const images = responseJSON.artifacts.map((image) =>
-                            `data:image/png;base64,${image.base64}`
-                        );
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
-            }
+			},
+            // preConfirm: (prompt) => {
+            //     return fetch(
+            //         `https://api.stability.ai/v1/generation/stable-diffusion-v1-5/text-to-image`,
+            //         {
+            //             method: 'POST',
+            //             headers: {
+            //                 'Content-Type': 'application/json',
+            //                 Accept: 'application/json',
+            //                 Authorization: `sk-bMhuMy55knbw133npsyeXP78HOe2TKMArEGlbueyxiAxaWED`,
+            //             },
+            //             body: JSON.stringify({
+            //                 text_prompts: [
+            //                     {
+            //                         text: prompt
+            //                     },
+            //                 ],
+            //                 cfg_scale: 7,
+            //                 clip_guidance_preset: 'FAST_BLUE',
+            //                 height: 512,
+            //                 width: 512,
+            //                 samples: 3,
+            //                 steps: 30,
+            //             }),
+            //         }
+            //     )
+            //         .then((response) => {
+            //             if (!response.ok) {
+            //                 throw new Error(`Non-200 response: ${response.text()}`);
+            //             }
+            //             return response.json();
+            //         })
+            //         .then((responseJSON) => {
+            //             const images = responseJSON.artifacts.map((image) =>
+            //                 `data:image/png;base64,${image.base64}`
+            //             );
+            //         })
+            //         .catch((error) => {
+            //             console.error(error);
+            //         });
+            // }
         })
     }
 
